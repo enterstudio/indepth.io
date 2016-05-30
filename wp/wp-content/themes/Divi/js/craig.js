24f9674b02129436e86ec4673952a78023f10339
Craig <craig.b.macgregor@gmail.com>
2016-05-30 20:10:36 +1200

    image switching code done, needs refinement with correct images though.

--- wp/wp-content/themes/Divi/js/craig.js
+++ wp/wp-content/themes/Divi/js/craig.js
@@ -1,1 +1,17 @@
-console.log('craig.js')
+(function($){
+  $(document).ready( function(){
+
+    var changeImage = function(index) {
+      $('.app-screen').css({display:'none'});
+      $('#app-screen-'+index).fadeIn();
+    }
+
+    $('#feature-1 h4').click(function() { changeImage(1); });
+    $('#feature-2 h4').click(function() { changeImage(2); });
+    $('#feature-3 h4').click(function() { changeImage(3); });
+    $('#feature-4 h4').click(function() { changeImage(4); });
+    $('#feature-5 h4').click(function() { changeImage(5); });
+    $('#feature-6 h4').click(function() { changeImage(6); });
+
+  });
+})(jQuery)
